4a8c32c3a7280b52775cb03c9649d7670dd160eb
pond-e <haruto2266@icloud.com>
2022-11-13 08:34:33 +0900

    add server_finish_wait

--- remember.py
+++ remember.py
@@ -1,8 +1,16 @@
 import subprocess
+import requests
+
+def server_finish_wait():# 記憶が成功してmainに処理が戻ったときに、まだserverでstete.txtが変更されてなくてもう一度このファイルが呼び出されるのを防ぐ
+    res = requests.get("https://tempet.sakura.tv/tempet/Raspi_state.txt")
+    while(res.content.decode() != "none"):# 
+        res = requests.get("https://tempet.sakura.tv/tempet/Raspi_state.txt")
 
 def main(remember_button):
+    temperature_bottom = 19
+    temperature_top = 29
     if(remember_button == "temperature"):
-        for i in range(19, 30):# 19℃から29℃のボタンを記憶する
+        for i in range(19, temperature_top+1):# 19℃から29℃のボタンを記憶する
             cmd = "python3 irrp.py -r -g18 -f temperature" + str(i) + " light:on --no-confirm --post 130"
             result = subprocess.run(cmd, capture_output=True, shell=True, text=True)
             if(result != ''):
@@ -11,7 +19,8 @@ def main(remember_button):
             else:
                 print("success temperature " + str(i))
                 # TODO:Raspi_receive.phpに{"status": "success"}をPOSTで送る
-                # TODO:記憶した情報を「記憶したボタン, 情報」の形でファイルに保存する（ファイル名未定）
+                if(i == temperature_top):
+                    server_finish_wait()
     else:
         cmd = "python3 irrp.py -r -g18 -f " + remember_button + " light:on --no-confirm --post 130"
         result = subprocess.run(cmd, capture_output=True, shell=True, text=True)
@@ -21,12 +30,15 @@ def main(remember_button):
         else:
             print("success " + remember_button)
             # TODO:Raspi_receive.phpに{"status": "success"}をPOSTで送る
-            # TODO:記憶した情報を「記憶したボタン, 情報」の形でファイルに保存する（ファイル名未定）
+            server_finish_wait()
+    
 
 
 def test(remember_button):
+    temperature_bottom = 19
+    temperature_top = 29
     if(remember_button == "temperature"):
-        for i in range(19, 30):# 19℃から29℃のボタンを記憶する
+        for i in range(19, temperature_top+1):# 19℃から29℃のボタンを記憶する
             cmd = "python3 irrp.py -r -g18 -f temperature" + str(i) + " light:on --no-confirm --post 130"
             result = subprocess.run(cmd, capture_output=True, shell=True, text=True)
             if(result != ''):
@@ -35,7 +47,8 @@ def test(remember_button):
             else:
                 print("success temperature " + str(i))
                 # TODO:Raspi_receive.phpに{"status": "success"}をPOSTで送る
-                # TODO:記憶した情報を「記憶したボタン, 情報」の形でファイルに保存する（ファイル名未定）
+                if(i == temperature_top):
+                    server_finish_wait()
     else:
         cmd = "python3 irrp.py -r -g18 -f " + remember_button + " light:on --no-confirm --post 130"
         result = subprocess.run(cmd, capture_output=True, shell=True, text=True)
@@ -45,8 +58,7 @@ def test(remember_button):
         else:
             print("success " + remember_button)
             # TODO:Raspi_receive.phpに{"status": "success"}をPOSTで送る
-            # TODO:記憶した情報を「記憶したボタン, 情報」の形でファイルに保存する（ファイル名未定）
-
+            server_finish_wait()
 
 if __name__ == "__main__":
     test("cooling")
