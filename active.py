4c697abacf4cc46cb80c7499d85bfa7c8025c2ce
pond-e <haruto2266@icloud.com>
2022-11-17 23:40:59 +0900

    add stop, active

--- active.py
+++ active.py
@@ -1,5 +1,15 @@
 import subprocess
 import re
+import requests
+import wpi3_bme280
 
 def main():
     print("active function")
+    
+    wpi3_bme280.readData()
+    temp = wpi3_bme280.temp
+    
+    r = requests.post("https://tempet.sakura.tv/tempet/Raspi_bme.php", data={'data': temp})
+    
+    pet = requests.get("https://tempet.sakura.tv/tempet/Raspi_selected_pet.txt")
+    preset = res.content.decode()
--- stop.py
+++ stop.py
@@ -3,3 +3,9 @@ import re
 
 def main():
     print("stop function")
+    cmd =  "python3 irrp.py -p -g17 -f "+"stop_button "+"light:on"
+    result = subprocess.run()
+    if(result!=''):
+        print("stop_buttonが失敗しました。")
+    else:
+        print("top_buttonが成功しました。")
