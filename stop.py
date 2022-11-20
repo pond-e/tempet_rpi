2ba166ff7758c4070ff68d08ee2bc53e81f08e77
pond-e <haruto2266@icloud.com>
2022-11-21 00:03:57 +0900

    fix

--- stop.py
+++ stop.py
@@ -4,7 +4,7 @@ import re
 def main():
     print("stop function")
     cmd =  "python3 irrp.py -p -g17 -f "+"stop_button "+"light:on"
-    result = subprocess.run()
+    result = subprocess.run(cmd, capture_output=True, shell=True, text=True)
     if(result!=''):
         print("stop_buttonが失敗しました。")
     else:
